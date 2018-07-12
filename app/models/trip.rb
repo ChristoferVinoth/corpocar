b4a79348405589527ecbb5d7f08d433412655b38
Vinoth Christofer <vinoth.christofer@freshworks.com>
2018-07-12 18:52:14 +0530

    change in association

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,6 @@
 class Trip < ActiveRecord::Base
-  has_one :driver, class_name: "User"
+  belongs_to :driver, class_name: "User"
   has_many :riders, through: :requests, class_name: "User"
   has_many :requests
   attr_accessible :driver_id, :origin, :destination, :start_time, :available_seats
 end
- 
